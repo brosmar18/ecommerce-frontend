250c75980f042a580836a3cd87c4e58d992a3fbb
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-12 16:15:06 -0500

    product name and description rendered

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -10,9 +10,9 @@ const HeroBanner = ({ heroBanner }) => {
     return (
         <div className='hero-banner'>
             <div>
-                <p className='hero-banner__s-text'>Small Text</p>
-                <h3 className='hero-banner__m-text'>Med Text</h3>
-                <h1 className='hero-banner__l-text'>Large Text</h1>
+                <p className='hero-banner__s-text'>{heroBanner.smallText}</p>
+                <h3 className='hero-banner__m-text'>{heroBanner.midText}</h3>
+                <h1 className='hero-banner__l-text'>{heroBanner.largeText1}</h1>
                 <img src={urlFor(heroBanner?.image)} alt='headphones' className='hero-banner__image' />
 
             </div>
@@ -20,8 +20,8 @@ const HeroBanner = ({ heroBanner }) => {
                 <button type='button' className='hero-banner__button'>Button Text</button>
             </Link>
             <div className='hero-banner__desc'>
-                <h5 className='hero-banner__desc-title'>Description</h5>
-                <p className='hero-banner__desc-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                <h5 className='hero-banner__desc-title'>{heroBanner.product}</h5>
+                <p className='hero-banner__desc-text'>{heroBanner.desc}</p>
             </div>
         </div>
     )
