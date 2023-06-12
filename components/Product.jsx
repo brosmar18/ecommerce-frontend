73d5075d992f0a04214f36d940c0ab8c81fa13cb
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-12 18:29:13 -0500

    Link, image and name section added

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import Link from 'next/link';
+import { urlFor } from '../library/client';
 
 const Product = () => {
     return (
-        <div>Product</div>
+        <div>
+            <Link href="#">
+                <div className='product'>
+                    <img
+                        src={urlFor(image && image[0])}
+                        width={250}
+                        height={250}
+                        className='product__image'
+                    />
+                    <p className='product__name'>Name</p>
+                </div>
+            </Link>
+        </div>
     )
 }
 
