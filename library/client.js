4dc8e69a53114376f7c117c5bd21743797cae3e0
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-11 21:04:25 -0500

    client setup completed

--- library/client.js
+++ library/client.js
@@ -1,10 +1,14 @@
 import sanityClient, { SanityClient } from '@sanity/client';
 import { ImageUrlBuilder } from '@sanity/image-url/lib/types/builder';
 
-const client = SanityClient({
-    projectId: '',
-    dataset: '',
-    apiVersion: '',
+export const client = SanityClient({
+    projectId: 'xziwqyjm',
+    dataset: 'production',
+    apiVersion: '2023-06-11',
     useDcn: true,
-    token: ''
-})
+    token: process.env.NEXT_PUBLIC_SANITY_TOKEN
+});
+
+const builder = ImageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
