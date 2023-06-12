91b82a5102b7b72f683e227480a905fdca011fc4
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-12 14:40:48 -0500

    button buit

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -11,6 +11,9 @@ const HeroBanner = () => {
                 <h3 className='hero-banner__m-text'>Med Text</h3>
                 <h1 className='hero-banner__l-text'>Large Text</h1>
             </div>
+            <Link href="#">
+                <button type='button' className='hero-banner__button'>Button Text</button>
+            </Link>
         </div>
     )
 }
