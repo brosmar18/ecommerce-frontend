529cbb933906ce23792939a5fedf698b778466b1
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-13 18:03:01 -0500

    getSTaticProps function setup

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-const ProductDetails = () => {
+const ProductDetails = ({ products, product }) => {
     return (
         <div>
             <div className='product-details'>
@@ -13,6 +13,18 @@ const ProductDetails = () => {
             </div>
         </div>
     )
+}
+
+export const getStaticProps = async ({params: { slug }}) => {
+    const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+    const productsQuery = '*[_type == "product"';
+
+    const product = await client.fetch(query);
+    const products = await client.fetch(productsQuery);
+
+    return {
+        props: { products, product }
+    }
 }
 
 export default ProductDetails;
