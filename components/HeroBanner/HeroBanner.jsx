f620c84694bf94bb9b99a2dee87214dd60041225
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-13 11:57:33 -0500

    HeroBanner class names updated to add styles

--- components/HeroBanner/HeroBanner.jsx
+++ components/HeroBanner/HeroBanner.jsx
@@ -3,27 +3,23 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import styles from './HeroBanner.module.scss';
-
 import { urlFor } from '../../library/client';
-
-
 
 const HeroBanner = ({ heroBanner }) => {
     return (
         <div className={styles['hero-banner']}>
             <div>
-                <p className='hero-banner__s-text'>{heroBanner.smallText}</p>
-                <h3 className='hero-banner__m-text'>{heroBanner.midText}</h3>
-                <h1 className='hero-banner__l-text'>{heroBanner.largeText1}</h1>
-                <img src={urlFor(heroBanner?.image)} alt='headphones' className='hero-banner__image' />
-
+                <p className={styles['hero-banner__s-text']}>{heroBanner.smallText}</p>
+                <h3 className={styles['hero-banner__m-text']}>{heroBanner.midText}</h3>
+                <h1 className={styles['hero-banner__l-text']}>{heroBanner.largeText1}</h1>
+                <img src={urlFor(heroBanner?.image)} alt='headphones' className={styles['hero-banner__image']} />
             </div>
             <Link href={`/product/${heroBanner.product}`}>
-                <button type='button' className='hero-banner__button'>{heroBanner.buttonText}</button>
+                <button type='button' className={styles['hero-banner__button']}>{heroBanner.buttonText}</button>
             </Link>
-            <div className='hero-banner__desc'>
-                <h5 className='hero-banner__desc-title'>{heroBanner.product}</h5>
-                <p className='hero-banner__desc-text'>{heroBanner.desc}</p>
+            <div className={styles['hero-banner__desc']}>
+                <h5 className={styles['hero-banner__desc-title']}>{heroBanner.product}</h5>
+                <p className={styles['hero-banner__desc-text']}>{heroBanner.desc}</p>
             </div>
         </div>
     )
