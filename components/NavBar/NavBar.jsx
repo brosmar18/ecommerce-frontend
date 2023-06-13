4d30495db320e8f2548d805ff707fb70e0a13513
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-13 15:54:53 -0500

    classes updated to add styles

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -6,13 +6,13 @@ import styles from './NavBar.module.scss';
 
 const NavBar = () => {
     return (
-        <div className='nav'>
-            <p className='nav__logo'>
+        <div className={styles['nav']}>
+            <p className={styles['nav__logo']}>
                 <Link href="/">Furry Pal News</Link>
             </p>
-            <button type='button' className='nav__cart-icon'>
+            <button type='button' className={styles['nav__cart-icon']}>
                 <AiOutlineShopping />
-                <span className='nav__item-qty'>1</span>
+                <span className={styles['nav__item-qty']}>1</span>
             </button>
         </div>
     )
