58b093ef17dbc14b20e132d3dcc616d787940abc
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-13 12:51:50 -0500

    styles updated

--- components/Product/Product.jsx
+++ components/Product/Product.jsx
@@ -6,20 +6,20 @@ import styles from './Product.module.scss';
 
 const Product = ({ products }) => {
     return (
-       <div className='products-container'>
-        {products?.map(({image, name, slug, price, _id}) => (
-            <Link key={_id} href={`/product/${slug.current}`}>
-                <div className='product'>
-                    <img
-                        src={urlFor(image && image[0])}
-                        className='product__image'
-                    />
-                    <p className='product__name'>{name}</p>
-                    <p className='product__price'>{price}</p>
-                </div>
-            </Link>
-        ))}
-       </div>
+        <div className={styles['products-container']}>
+            {products?.map(({ image, name, slug, price, _id }) => (
+                <Link key={_id} href={`/product/${slug.current}`}>
+                    <div className={styles['product']}>
+                        <img
+                            src={urlFor(image && image[0])}
+                            className={styles['product__image']}
+                        />
+                        <p className={styles['product__name']}>{name}</p>
+                        <p className={styles['product__price']}>${price}</p>
+                    </div>
+                </Link>
+            ))}
+        </div>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,6 @@ const Home = ({ products, bannerData }) => (
         <h2 className="products-heading__title">Best Seller Products</h2>
         <p className="products-heading__desc">Description of products or selling statement</p>
       </div>
-
-      {/* <div className="products-container">
-        {products?.map((product) => <Product key={product._id} product={product} />)}
-      </div> */}
       <Product products={products}/>
 
       <FooterBanner />
