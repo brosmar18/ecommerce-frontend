7e13139780d2c46095a3df697c7c2b293fc05d98
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-13 11:43:23 -0500

    file paths updated

--- components/HeroBanner/HeroBanner.jsx
+++ components/HeroBanner/HeroBanner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { urlFor } from '../library/client';
+import { urlFor } from '../../library/client';
 
 
 
--- components/index.js
+++ components/index.js
@@ -1,7 +1,7 @@
-export { default as Footer } from './Footer';
-export { default as Cart } from './Cart';
-export { default as NavBar } from './NavBar';
-export { default as FooterBanner } from './FooterBanner';
-export { default as HeroBanner } from './HeroBanner';
-export { default as Layout } from './Layout';
-export { default as Product } from './Product';
+export { default as Footer } from './Footer/Footer';
+export { default as Cart } from './Cart/Cart';
+export { default as NavBar } from './NavBar/NavBar';
+export { default as FooterBanner } from './FooterBanner/FooterBanner';
+export { default as HeroBanner } from './HeroBanner/HeroBanner';
+export { default as Layout } from './Layout/Layout';
+export { default as Product } from './Product/Product';
