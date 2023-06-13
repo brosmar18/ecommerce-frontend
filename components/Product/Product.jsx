445020599f4be54601437bd3793c8d272ed9e436
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-13 12:07:46 -0500

    styles moved to .scss file

--- components/Product/Product.jsx
+++ components/Product/Product.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Link from 'next/link';
 import { urlFor } from '../../library/client';
 
+import styles from './Product.module.scss';
+
 const Product = ({ product: {image, name, slug, price }}) => {
     return (
         <div>
