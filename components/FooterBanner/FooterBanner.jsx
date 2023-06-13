f5cbcef4697cf33ba665f4b15e464bc05aa41c67
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-13 13:45:16 -0500

    largeText2 render

--- components/FooterBanner/FooterBanner.jsx
+++ components/FooterBanner/FooterBanner.jsx
@@ -12,7 +12,7 @@ const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTi
                 <div className={styles['footer-banner__left']}>
                     <p>{discount}</p>
                     <h3>{largeText1}</h3>
-                    <h3>Large Text 2</h3>
+                    <h3>{largeText2}</h3>
                     <p>Sale Time</p>
                 </div>
                 <div className={styles['footer-banner__right']}>
