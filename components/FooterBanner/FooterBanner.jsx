0d1b28a7ca409adf2f974772cd576f9bf8bad06a
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-13 13:22:13 -0500

    Left and right banner sections created

--- components/FooterBanner/FooterBanner.jsx
+++ components/FooterBanner/FooterBanner.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import Link from 'next/link';
+
+import styles from './FooterBanner.module.scss';
+import { urlFor } from '../../library/client';
 
 
-const FooterBanner = () => {
+const FooterBanner = ({ footerBanner }) => {
     return (
-        <div>Footer Banner</div>
+        <div className={styles['footer-banner']}>
+            <div className={styles['footer-banner__desc']}>
+                <div className={styles['footer-banner__left']}>
+                    <p>Discount</p>
+                    <h3>Large Text 1</h3>
+                    <h3>Large Text 2</h3>
+                    <p>Sale Time</p>
+                </div>
+                <div className={styles['footer-banner__right']}>
+                    <p>Small Text</p>
+                    <h3>Mid Text</h3>
+                    <p>Description</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
