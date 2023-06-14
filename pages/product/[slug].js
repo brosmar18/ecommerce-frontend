e594bdd06cc74a27c2162633eb38653f6326f7d2
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-14 16:17:04 -0500

    useStateContext setup

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import client, { urlFor } from '../../library/client';
 import { Product } from '../../components';
+import {useStateContext} from '../../context/StateContext';
 
 import styles from './[slug].module.scss';
 
