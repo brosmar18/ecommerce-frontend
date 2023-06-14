d59e42d6d080ad01abe2c0066d9796d8adcd0d19
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-14 16:10:41 -0500

    React imported

--- context/StateContext.js
+++ context/StateContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 import { toast } from 'react-hot-toast';
 
 const Context = createContext();
