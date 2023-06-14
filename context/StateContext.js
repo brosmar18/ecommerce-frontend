8f31720b02c789f0e7af141b9b85a383a1e0a2f0
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-14 16:27:59 -0500

    onAdd function updated

--- context/StateContext.js
+++ context/StateContext.js
@@ -11,8 +11,22 @@ export const StateContext = ({ children }) => {
     const [totalQuantities, setTotalQuantities] = useState(0);
     const [qty, setQty] = useState(1);
 
-    const onAdd = () => {
-        
+    const onAdd = (product, quantity) => {
+        const checkProductInCart = cartItems.find((item) => item._id === product._id);
+
+        if(checkProductInCart) {
+            setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
+            setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+
+            const updatedCartItems = cartItems.map((cartProduct) => {
+                if(cartProduct._id === product._id) return {
+                    ...cartProduct,
+                    quantity: cartProduct.quantity + quantity
+                }
+            })
+            setCartItems(updatedCartItems);
+            toast.success(`${qty} ${product.name} added to the cart.`);
+        }
     }
 
     const incQty = () => {
