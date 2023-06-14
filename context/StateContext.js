918f9eb9e909b86165087b581eb5714579b9c561
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-14 16:02:36 -0500

    useStates setup

--- context/StateContext.js
+++ context/StateContext.js
@@ -3,6 +3,13 @@ import { toast } from 'react-hot-toast';
 
 const Context = createContext();
 
-export const StateContext = () => {
+
+export const StateContext = ({ children }) => {
+    const [showCart, setShowCart] = useState(false);
+    const [cartItems, setCartItems] = useState([]);
+    const [totalPrice, setTotalPrice] = useState(0);
+    const [totalQuantities, setTotalQuantities] = useState(0);
+    const [qty, setQty] = useState(1);
+
     
 }
