5d1e9a75013dc577de5ec74b53232efcb86d1656
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-14 16:00:15 -0500

    basic func component created

---
+++ context/StateContext.js
@@ -0,0 +1,8 @@
+import { createContext, useContext, useState, useEffect } from 'effect';
+import { toast } from 'react-hot-toast';
+
+const Context = createContext();
+
+export const StateContext = () => {
+    
+}
