7b67cdbb72dd1cd862d257b6edb6d165f0b5a74e
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-14 16:56:01 -0500

    Cart button added with OutlineLeft logo

--- components/Cart/Cart.jsx
+++ components/Cart/Cart.jsx
@@ -6,14 +6,23 @@ import { Toast } from 'react-hot-toast';
 
 import { useStateContext } from '../../context/StateContext';
 import { urlFor } from '../../library/client';
+import styles from './Cart.module.scss';
 
 const Cart = () => {
     const cartRef = useRef();
     const { totalPrice, totalQuantities, cartItems, setShowCart } = useStateContext();
 
-    
+
     return (
-        <>Cart</>
+        <div className={styles.cartWrapper} ref={cartRef}>
+            <div className={styles.cartContainer}>
+                <button type='button' className={styles.cartHeading} onClick={() => setShowCart(false)}>
+                    <AiOutlineLeft />
+                    <span className={styles.heading}>Your Cart</span>
+                    <span className={styles.cartNumItems}>({totalQuantities})</span>
+                </button>
+            </div>
+        </div>
     )
 }
 
