85a6e188b3ef1f46b3a3d89bf610505679a8d8f4
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-15 10:07:19 -0500

    setTotalPrice setup

--- context/StateContext.js
+++ context/StateContext.js
@@ -43,11 +43,14 @@ export const StateContext = ({ children }) => {
     const toggleCartItemQuantity = (id, value) => {
         foundProduct = cartItems.find((item) => item._id === id);
         index = cartItems.findIndex((product) => product._id === id);
-        const newCartItem = cartItems.filter((item) => item._id !== id)
+        // const newCartItems = cartItems.filter((item) => item._id !== id)
 
         if(value === 'inc') {
             setCartItems([...newCartItems, {...foundProduct, quantity: foundProduct.quantity + 1 }]);
-        } else if ( value === 'dec')
+            setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price);
+        } else if ( value === 'dec') {
+
+        }
     }
 
     const incQty = () => {
